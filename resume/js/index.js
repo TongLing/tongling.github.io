853e9c8c13e9ae35e85034477cd9b39c5090247e
TongLing <545716730@qq.com>
2016-06-08 14:03:13 +0800

    last revise

--- resume/js/index.js
+++ resume/js/index.js
@@ -1,3 +1,79 @@
+        var RollFlag = 1;
+		var scrollFunc = function (e) {
+        e = e || window.event;
+        if (e.wheelDelta && RollFlag) {  //判断浏览器IE，谷歌滑轮事件             
+            if (e.wheelDelta > 0) { //当滑轮向上滚动时
+               
+               Rollup();
+                RollFlag=0;
+            }
+            if (e.wheelDelta < 0) { //当滑轮向下滚动时
+               Rolldown();
+                RollFlag=0;
+            
+            }
+        } else if (e.detail) {  //Firefox滑轮事件
+            if (e.detail> 0) { //当滑轮向上滚动时
+               Rollup();
+                RollFlag=0;
+            }
+            if (e.detail< 0) { //当滑轮向下滚动时
+                Rolldown();
+                RollFlag=0;
+                 
+            }
+        }
+    }
+    //给页面绑定滑轮滚动事件
+    if (document.addEventListener) {//firefox
+        document.addEventListener('DOMMouseScroll', scrollFunc, false);
+    }
+    //滚动滑轮触发scrollFunc方法  //ie 谷歌
+    window.onmousewheel = document.onmousewheel = scrollFunc; 
+
+function Rollup(){
+    var num = -1;
+    //计算出当前的num位置
+    var par = document.getElementById("content");
+    var a = par.childNodes[1];
+    var num = 0;
+    do{
+
+        if (a.getAttribute("class") === "show") {break;}
+        else {
+            a=a.nextElementSibling;
+            num++;
+        }
+    }while(a.nextElementSibling !== null)
+    if (num === 0) return;
+    else{
+        switchMenu(num-1);
+        console.log(num);
+        return;
+    }
+}
+
+function Rolldown(){
+    var num = -1;
+    //计算出当前的num位置
+    var par = document.getElementById("content");
+    var a = par.childNodes[1];
+    var num = 0;
+    do{
+
+        if (a.getAttribute("class") === "show") {break;}
+        else {
+            a=a.nextElementSibling;
+            num++;
+        }
+    }while(a.nextElementSibling !== null)
+    if (num === 5) return;
+    else{
+        switchMenu(num+1);
+        console.log(num);
+        return;
+    }
+}
 
 function switchContent(num) {
     var par = document.getElementById("content");
@@ -16,8 +92,6 @@ function switchContent(num) {
     }
     b.setAttribute("class","show");
 }
-
-
 function switchNav(num) {
     var len = document.getElementsByClassName("menu").length;
     for (var i = 0; i < len; i++)
